4e74ee581b668b83ff6c51c708801e7bc28c037b
Mariana Naselli <nasellimariana@gmail.com>
2023-02-07 15:27:29 +0100

    Agrego aside

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
-menuEmail.addEventListener('click', toogleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toogleDesktopMenu(){
+
+function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu (){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside (){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    //abrir el aside  u esta open hay que cerrarlo
+
+    // cada vez que querramos abrir el carrito cerramos ell menu mobile
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+     aside.classList.toggle('inactive');
+
+     
+}
+
+
 
